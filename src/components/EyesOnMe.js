5d30e1bab8c70a019df58ae78127585e12c6d612
Axel Aquino <aquino.axel@gmail.com>
2022-02-24 12:03:18 -0500

    commit tree eafa3edb0a54a895976ad3ba082d31a3e211e365

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+function Focus() {
+  return console.log("Good!");
+}
+
+function Blur() {
+  return console.log("Hey! Eyes on me!");
+}
+
+function EyesOnMe() {
+  return (
+    <button onFocus={Focus} onBlur={Blur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+
+import React from "react";
+
+function Keypad() {
+  //     function changeHandler(event) {
+  //     return console.log(event.target.value);
+  //   }
+
+  return (
+    <input
+      type="password"
+      //   onChange={changeHandler}
+
+      onChange={() => console.log("Entering password...")}
+    ></input>
+  );
+}
+
+export default Keypad;
